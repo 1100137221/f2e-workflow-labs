c9fe4a3c8f2e86ecfceb05296a7d882f9ad5d1eb
Will <doggy.huang@gmail.com>
2015-10-24 16:11:23 +0800

    17 建立 clean 工作，使用 del 模組刪除 output2/bootstrap/** 所有檔案，但要確保 bootstrap 資料夾不被刪除

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 var gulp = require('gulp');
+var del = require('del');
 
 gulp.task('default', ['mytask1', 'mytask2'], function() {
 	console.log('Hello default task');
@@ -19,7 +20,7 @@ gulp.task('output1', function() {
 		.pipe(gulp.dest('output1'));
 });
 
-gulp.task('output2', function() {
+gulp.task('output2', ['clean'], function() {
 	gulp
 		.src('assets/vendor/bootstrap/**/*.js',
 			{
@@ -35,4 +36,11 @@ gulp.task('output3', function() {
 				'assets/vendor/**/*.css'
 			])
 		.pipe(gulp.dest('output3'));
+});
+
+gulp.task('clean', function(cb) {
+	del(['output2/bootstrap/**', '!output2/bootstrap']).then(function (paths) {
+		console.log('Deleted files/folders:\n', paths.join('\n'));
+		cb();
+	});
 });
