d8bafcc7e4926e839b7664bc8f14d881f867a476
Will <doggy.huang@gmail.com>
2015-10-25 11:47:46 +0800

    28 使用 gulp-load-plugins 簡化 Gulp 外掛載入過程

--- gulp/tasks/app.js
+++ gulp/tasks/app.js
@@ -1,16 +1,8 @@
 var gulp = require('gulp');
 var del = require('del');
+var $ = require('gulp-load-plugins')();
 
 var config = require('../config');
-	
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var rename = require('gulp-rename');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss  = require('gulp-minify-css');
-var less = require('gulp-less');
-var path = require('path');
-var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('watch', function() {
 	gulp
@@ -24,39 +16,39 @@ gulp.task('app', function() {
 		])
 		.pipe(gulp.dest(config.assetsDir + '/src'))
 		
-		.pipe(sourcemaps.init())
+		.pipe($.sourcemaps.init())
 		
-			.pipe(concat('app.js'))
+			.pipe($.concat('app.js'))
 			.pipe(gulp.dest(config.assetsDir + ''))
 
-			.pipe(uglify(config.uglifyOptions))
-			.pipe(rename({
+			.pipe($.uglify(config.uglifyOptions))
+			.pipe($.rename({
 				extname: '.min.js'
 			}))
 			
-		.pipe(sourcemaps.write('./'))
+		.pipe($.sourcemaps.write('./'))
 		
 		.pipe(gulp.dest(config.assetsDir + ''));
 		
 	gulp.src(['index.html'])
-		.pipe(minifyHtml())
-		.pipe(rename({
+		.pipe($.minifyHtml())
+		.pipe($.rename({
 			extname: '.min.html'
 		}))
 		.pipe(gulp.dest('./'));
 
 	gulp.src([config.assetsDir + '/styles.css'])
-		.pipe(minifyCss(config.cssOptions))
-		.pipe(rename({
+		.pipe($.minifyCss(config.cssOptions))
+		.pipe($.rename({
 			extname: '.min.css'
 		}))
 		.pipe(gulp.dest(config.assetsDir + '/'));
 		
 	gulp.src([config.assetsDir + '/styles.less'])
-		.pipe(less())
+		.pipe($.less())
 		.pipe(gulp.dest('less_to_css/'))
-		.pipe(minifyCss(config.cssOptions))
-		.pipe(rename({
+		.pipe($.minifyCss(config.cssOptions))
+		.pipe($.rename({
 			extname: '.min.css'
 		}))
 		.pipe(gulp.dest('less_to_css/'));
--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,4 @@
 var gulp = require('./gulp');
 
-module.exports = require('gulp');
+// 這行程式碼看起來沒啥用，我直接註解掉
+//module.exports = require('gulp');
