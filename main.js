a10d801911140542fe50e4298bed50081ece89fa
Will <doggy.huang@gmail.com>
2015-10-25 15:16:26 +0800

    32 使用 url-loader 載入圖片，預設採用 data URI 型態顯示圖片，但要設定當檔案大小大於 8KB 時自動改用實體圖片載入

--- main.js
+++ main.js
@@ -7,7 +7,12 @@ console.log('Hello world! assets Path: ' + config.assetsDir);
 $(function() {
 	
 	$('body').on('click', function() {
-		require('style!css!./assets/styles.css');
+		require('./assets/styles.css');
+		
+		var picurl = require('./Avatar.jpg');
+		 
+		$('#pic').append('<img src="'+picurl+'">')
+		
 	});
 	
 });
