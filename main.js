7665222c8d1bb29e042aeb78c6dcbd81a7e4dd9e
Will <doggy.huang@gmail.com>
2015-10-25 14:37:02 +0800

    31 使用 css-loader 與 style-loader 將 *.css 視為模組載入

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
+var $ = require('jquery');
 
 var config = require('./gulp/config');
 
 console.log('Hello world! assets Path: ' + config.assetsDir);
+
+$(function() {
+	
+	$('body').on('click', function() {
+		require('style!css!./assets/styles.css');
+	});
+	
+});
